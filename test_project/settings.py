94c79ef26dadcf5025494783af9ccfd0b56b0769
mvijay@2 <vijjuvolley1@gmail.com>
2025-04-29 15:26:25 +0530

    added csrftrustedorigin in settings

--- test_project/settings.py
+++ test_project/settings.py
@@ -28,6 +28,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://jalodhari.wassan.org',
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
